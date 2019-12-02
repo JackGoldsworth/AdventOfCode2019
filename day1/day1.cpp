442bbdad03e125535853bca89bd93bbddee3f6f9
JackGoldsworth <jackgoldsworth@gmail.com>
2019-12-02 00:29:46 -0600

    Day 2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,14 +4,15 @@
 
 using namespace std;
 
-int* loadMass();
+int *loadMass();
+
 int getFuelCost(int fuel);
 
 int main() {
-    int* masses = loadMass();
+    int *masses = loadMass();
     unsigned int sum = 0;
     unsigned int secondPartSum = 0;
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++) {
         sum += masses[i] / 3 - 2;
         secondPartSum += getFuelCost(masses[i]);
     }
@@ -20,19 +21,18 @@ int main() {
     return 0;
 }
 
-int* loadMass() {
+int *loadMass() {
     static int masses[100];
     ifstream file;
     file.open(R"(U:\CLionProjects\Advent2019\day1\input.txt)");
-    if(!file) {
+    if (!file) {
         cout << "Unable to open the input file.";
         exit(1);
     }
     string line;
     int counter = 0;
-    while(getline(file, line)) {
+    while (getline(file, line)) {
         masses[counter] = stoi(line);
-        cout << line << endl;
         counter++;
     }
     file.close();
@@ -42,7 +42,7 @@ int* loadMass() {
 int getFuelCost(int fuel) {
     static int fuelSum;
     int fuelCost = fuel / 3 - 2;
-    if(fuelCost <= 0) {
+    if (fuelCost <= 0) {
         int temp = fuelSum;
         fuelSum = 0;
         return temp;
---
+++ day2/day2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int *loadOpCodes();
+
+int getFuelCost(int fuel);
+
+void part1();
+
+void part2();
+
+int main() {
+    part1();
+    part2();
+    return 0;
+}
+
+void part1() {
+    int *opCodes = loadOpCodes();
+    for (int i = 0; i < 200; i += 4) {
+        cout << "Position " << i << ": " << opCodes[i] << endl;
+        int place = opCodes[i + 3];
+        int add1 = opCodes[i + 1];
+        int add2 = opCodes[i + 2];
+        if (opCodes[i] == 1) {
+            opCodes[place] = opCodes[add1] + opCodes[add2];
+        } else if (opCodes[i] == 2) {
+            opCodes[place] = opCodes[add1] * opCodes[add2];
+        } else if (opCodes[i] == 99) {
+            cout << "Position 0: " << opCodes[0] << endl;
+            return;
+        }
+    }
+}
+
+void part2() {
+    for (int x = 0; x <= 99; x++) {
+        for (int y = 0; y <= 99; y++) {
+            int *opCodes = loadOpCodes();
+            cout << "X: " << x << " Y: " << y << endl;
+            for (int i = 0; i < 200; i += 4) {
+                if (i == 0) {
+                    opCodes[1] = x;
+                    opCodes[2] = y;
+                }
+                int place = opCodes[i + 3];
+                int add1 = opCodes[i + 1];
+                int add2 = opCodes[i + 2];
+                if (opCodes[i] == 1) {
+                    opCodes[place] = opCodes[add1] + opCodes[add2];
+                } else if (opCodes[i] == 2) {
+                    opCodes[place] = opCodes[add1] * opCodes[add2];
+                } else if (opCodes[i] == 99) {
+                    if (opCodes[0] == 19690720) {
+                        cout << "Position 0: " << 100 * x + y << endl;
+                        exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int *loadOpCodes() {
+    static int opCodes[200];
+    ifstream file;
+    file.open(R"(U:\CLionProjects\Advent2019\day2\input.txt)");
+    if (!file) {
+        cout << "Unable to open the input file.";
+        exit(1);
+    }
+    string line;
+    int counter = 0;
+    while (getline(file, line, ',')) {
+        opCodes[counter] = stoi(line);
+        counter++;
+    }
+    file.close();
+    return opCodes;
+}
